79d1e968634a74d945d231558d22313ebf40b472
Shorno <fb.shorno@gmail.com>
2026-05-31 21:09:43 +0600

    dp

--- dp/index.ts
+++ dp/index.ts
@@ -1,18 +1,23 @@
-function checkSum(number: number, num_arr: number[]) {
+function checkSum(number: number, num_arr: number[], memo: Record<number, boolean>= {}) {
 
+    if (number in memo) return memo[number];
     if (number < 0) return false;
     if(number === 0 ) return true;
 
     for (const num of num_arr){
-        const remaning = number - num;
+        const remaining = number - num;
 
 
-        if(checkSum(remaning, num_arr) === true) return true;
+        if(checkSum(remaining, num_arr, memo)){
+            memo[number] = true;
+            return  true;
+        }
     }
 
+    memo[number] = false;
     return false;
 
 }
 
-
-const res = checkSum(8, [3, 6, 7])
+const res = checkSum(50, [3,3,3])
+console.log(res)
